071d8faddbb79e5a1a28cea772feaae60ce81739
vedavamadathil <vesion4690@gmail.com>
2021-07-12 12:10:40 -0700

    Finally figured out multiprocessing with TF

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 import gym
 
 import numpy as np
-import tensorflow as tf
+import matplotlib.pyplot as plt
 
-from tensorflow import keras
 from collections import deque
+from pathos.multiprocessing import ProcessPool
 
 from scheduler import *
 from heurestics import *
 
+'''
+'''
+
+'''
+# Model skeletons
 models = {
     # Taken from hands-on-machine learning
     'CartPole-v1': keras.models.Sequential([
@@ -16,7 +21,7 @@ models = {
         keras.layers.Dense(32, activation="elu"),
         keras.layers.Dense(2)
     ])
-}
+}'''
 
 def sample(rbf, size):
     indices = np.random.randint(len(rbf), size = size)
@@ -30,10 +35,27 @@ def sample(rbf, size):
 
     return states, actions, rewards, nstates, dones
 
-def run(skeleton, ename, heurestic, scheduler, episodes, steps):
+# If a skeleton becomes necessary use a list of some named tuples, or smthing
+def run(ename, heurestic, scheduler, episodes, steps):
+    import tensorflow as tf
+    from tensorflow import keras
+
+    # Config GPUs
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+
+    model = keras.models.Sequential([
+        keras.layers.Dense(32, activation="elu", input_shape=[4]),
+        keras.layers.Dense(32, activation="elu"),
+        keras.layers.Dense(2)
+    ])
+
     # Setup the model and environment
-    model = keras.models.clone_model(skeleton)
-    model.set_weights(skeleton.get_weights())
+    # model = keras.models.load_model(path)
+    # model = keras.models.clone_model(skeleton)
+    # model.set_weights(skeleton.get_weights())
 
     # Other variables
     env = gym.make(ename)
@@ -42,9 +64,11 @@ def run(skeleton, ename, heurestic, scheduler, episodes, steps):
     batch_size = 32             # Should be customizable
 
     gamma = 0.95                # Should be customizable
-    loss = keras.losses.mean_squared_error
+    loss_ftn = keras.losses.mean_squared_error
     optimizer = keras.optimizers.Adam(learning_rate = 1e-2)
-    nouts = 1                   # Should be customizable
+    nouts = env.action_space.n  # Should be customizable
+
+    scores = []
 
     # Training loop
     for e in range(episodes):
@@ -60,7 +84,7 @@ def run(skeleton, ename, heurestic, scheduler, episodes, steps):
                 Qvs = model(state[np.newaxis])
                 # print the Q_values
                 action = np.argmax(Qvs[0])
-            
+
             # Apply the action and update the state
             nstate, reward, done, info = env.step(action)
             rbf.append((state, action, reward, nstate, done))
@@ -69,10 +93,11 @@ def run(skeleton, ename, heurestic, scheduler, episodes, steps):
 
             if done:
                 break
-        
+
         # Progress the scheduler
+        print(f'Episode {e}, Score = {score}, Epsilon {eps}')
+        scores.append(score)
         eps = scheduler()
-        print('Score = ', score)
 
         # Train the model if the rbf is full enough
         if len(rbf) >= batch_size:
@@ -88,9 +113,24 @@ def run(skeleton, ename, heurestic, scheduler, episodes, steps):
             with tf.GradientTape() as tape:
                 all_Qvs = model(states)
                 Qvs = tf.reduce_sum(all_Qvs * mask, axis = 1, keepdims = True)
-                loss = tf.reduce_mean(loss(tgt_Qvs, Qvs))
+                loss = tf.reduce_mean(loss_ftn(tgt_Qvs, Qvs))
 
             grads = tape.gradient(loss, model.trainable_variables)
             optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-run(models['CartPole-v1'], 'CartPole-v1', Heurestic('Great HR', theta_omega), LinearDecay(400), 10, 500)
+    return scores
+
+# run(models['CartPole-v1'], 'CartPole-v1', Heurestic('Egreedy', theta_omega),
+#        DampedOscillator(400, 50), 1000, 500)
+# models['CartPole-v1'].save('cv1.h5')
+
+envs = ['CartPole-v1', 'CartPole-v1']
+hrs = [Heurestic('Great HR', theta_omega), Heurestic('Egreedy', egreedy)]
+schs = [DampedOscillator(400, 50), DampedOscillator(400, 50)]
+episodes = [1000, 1000]
+steps = [500, 500]
+
+pool = ProcessPool(2)
+results = pool.map(run, envs, hrs, schs, episodes, steps)
+
+print('results: ', results)
