7205ef4a234511613733344f34123932664e20d1
Venkataram Edavamadathil Sivaram <42660556+vedavamadathil@users.noreply.github.com>
2021-07-29 19:21:56 -0700

    Updating graph methods

--- graph.py
+++ graph.py
@@ -4,58 +4,97 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import scipy
 
 sns.set_theme()
-def graph_file(path):
+def get_trial(path):
+    # print(f'path = {path}')
+    fout = open(path, 'r')
+    lines = fout.readlines()
+    epsilons = [float(s) for s in (lines[0][:-1].split(','))[1:]]
+    scores = [float(s) for s in (lines[1][:-1].split(','))[1:]]
+    finals = [float(s) for s in (lines[2][:-1].split(','))[1:]]
+    return epsilons, scores, finals
+
+def get_ts_trial(path):
+    # print(f'path = {path}')
+    fout = open(path, 'r')
+    lines = fout.readlines()
+    epsilons = [float(s) for s in (lines[0][:-1].split(','))[1:]]
+    kepsilons = [float(s) for s in (lines[1][:-1].split(','))[1:]]
+    scores1 = [float(s) for s in (lines[2][:-1].split(','))[1:]]
+    scores2 = [float(s) for s in (lines[3][:-1].split(','))[1:]]
+    finals1 = [float(s) for s in (lines[4][:-1].split(','))[1:]]
+    finals2 = [float(s) for s in (lines[5][:-1].split(','))[1:]]
+    return epsilons, kepsilons, scores1, scores2, finals1, finals2
+
+def get_policy(path):
+    out = []
+    trials = [tf for tf in os.listdir(path)]
+    for tr in trials:
+        eps, scs, fin = get_trial(path + '/' + tr)
+        out.append(scs)
+    return out
+
+# Take only the first agents score (by symmetry it should also
+# at least somewhat represent the other agent)
+def get_ts_policy(path):
+    out = []
+    trials = [tf for tf in os.listdir(path)]
+    for tr in trials:
+        eps, keps, scs1, scs2, fin1, fin2 = get_ts_trial(path + '/' + tr)
+        out.append(scs1)
+    return out
+
+def get_policy_average(path, pol):
+    # Variables
+    conf = 0.95
+    window = 10
+
+    if pol == 'TS_Tutoring':
+        tscores = get_ts_policy(path + '/' + pol)
+    else:
+        tscores = get_policy(path + '/' + pol)
+
+    tscores_avg = np.average(tscores, axis = 0)
+    tscores_sem = scipy.stats.sem(tscores, axis = 0)
+
+    n = len(tscores)
+    h = tscores_sem * scipy.stats.t.ppf((1 + conf) / 2.0, n-1)
+
+    # Smooth out both
+    tscores_avg = np.convolve(tscores_avg, np.ones(window), 'valid') / window
+    h = np.convolve(h, np.ones(window), 'valid') / window
+
+    return tscores_avg, h
+
+def graph_policy(path):
+    # Trial files
+    trials = [tf for tf in os.listdir(path)]
+
     # Create the subplot (and change the theme)
     fig = plt.figure(figsize = (8, 6))
 
     scores = fig.add_subplot(3, 1, (1, 2))
     eps = fig.add_subplot(3, 1, 3)
 
-    spi = 0
-    for i in range(len(path)):
-        if path[i] == '/':
-            spi = i
+    print(trials)
 
-    title = path[spi + 1:-4].replace('_', ' ')
-    fig.suptitle(f'Statistics For {title}')
+    epsilons = []
+    ascores = []
+    for tf in trials:
+        teps, scs, finals = get_trial(path + '/' + tf)
+        ascores.append(scs)
+        epsilons.append(teps)
 
-    # Read the file
-    file = open(path)
-    lines = [line[:-1] for line in file.readlines()]
+    # Plotting the scores
+    size = len(ascores[0])
+    episodes = range(1, size + 1)
+    for i in range(len(ascores)):
+        scores.plot(episodes, ascores[i], label = trials[i][:-4].replace('_', ' '))
 
-    final = 0
-    while lines[final][0] != 'B': final += 1
-
-    # Collect episodes
-    episodes = lines[0].split(',')
-    epsilons = lines[1].split(',')
-    episodes = [float(s) for s in episodes[1:]]
-
-    for line in lines[2 : final]:
-        split = line.split(',')
-
-        label = split[0]
-        values = split[1:]
-
-        scores.plot(episodes, [float(s) for s in values], label = label)
-
-    feps = lines[final].split(',')
-    feps = [float(s) for s in feps[1:]]
-
-    benchs = []
-    for line in lines[final + 1:]:
-        split = line.split(',')
-
-        label = split[0]
-        values = split[1:]
-
-        benchs.append(values[0])
-
-    print('Bench trials:', benchs)
-
-    eps.plot(episodes, [float(s) for s in epsilons[1:]], label = 'Epsilon')
+    # Plotting the epsilons
+    eps.plot(episodes, epsilons[0], label = 'Epsilon')
 
     # Axis labels
     scores.set_ylabel('Scores')
@@ -79,91 +118,76 @@ def graph_file(path):
         fig.savefig(loc)
     print()
 
-def file_averages(path):
-    # Read the file
-    file = open(path)
-    lines = [line[:-1] for line in file.readlines()]
+def graph_ts_policy(path):
+    # Trial files
+    trials = [tf for tf in os.listdir(path)]
 
-    final = 0
-    while lines[final][0] != 'B': final += 1
-
-    trials = [
-        [float(s) for s in (line.split(','))[1:]] for line in lines[2:final - 1]
-    ]
-
-    finals = [
-        [float(s) for s in (line.split(','))[1:]] for line in lines[final + 1:]
-    ]
-
-    trials_average = np.average(trials, axis = 0).tolist()
-    finals_average = np.average(finals, axis = 0).tolist()
-
-    policy = path[path.rindex('/') + 1: -4]
-    print('\n' + policy + ', average training score:', np.average(trials_average))
-    print(policy + ', average bench score:', np.average(finals_average))
-
-    return (trials_average, finals_average)
-
-def tutoring_averages(path):
-    # Read the file
-    file = open(path)
-    lines = [line[:-1] for line in file.readlines()]
-
-    final = 0
-    while lines[final][0] != 'B': final += 1
-
-    trials = [
-        [float(s) for s in (line.split(','))[1:]] for line in lines[2:final - 1]
-    ]
-
-    finals_average = [
-        float(line.split(',')[1]) for line in lines[final:]
-    ]
-
-    print('finals: ', finals_average)
-
-    trials_average = np.average(trials, axis = 0).tolist()
-
-    policy = path[path.rindex('/') + 1: -4]
-    print('\n' + policy + ', average training score:', np.average(trials_average))
-    print(policy + ', average bench score:', np.average(finals_average))
-
-    return trials_average
-
-def graph_full_environment(path, files):
-    # Setup the figure
-    plt.style.use('ggplot')
-
+    # Create the subplot (and change the theme)
     fig = plt.figure(figsize = (8, 6))
 
+    scores = fig.add_subplot(3, 1, (1, 2))
+    eps = fig.add_subplot(3, 1, 3)
+
+    print(trials)
+
+    epsilons = []
+    ascores = []
+    for tf in trials:
+        # TODO: print statistics about the finals
+        teps, tkeps, scs1, scs2, f1, f2 = get_ts_trial(path + '/' + tf)
+        ascores.append((scs1, scs2))
+        epsilons.append((teps, tkeps))
+
+    # Plotting the scores
+    size = len(ascores[0][0])
+    episodes = range(1, size + 1)
+    for i in range(len(ascores)):
+        scores.plot(episodes, ascores[i][0],
+            label = trials[i][:-4].replace('_', ' ') + ' (A1)')
+        scores.plot(episodes, ascores[i][1],
+            label = trials[i][:-4].replace('_', ' ') + ' (A2)')
+
+    # Plotting the epsilons
+    eps.plot(episodes, epsilons[0][0], label = 'Epsilon')
+    eps.plot(episodes, epsilons[0][1], label = 'K-epsilon')
+
+    # Axis labels
+    scores.set_ylabel('Scores')
+    eps.set_ylabel('Epsilon')
+    eps.set_xlabel('Episodes')
+
+    # Legends
+    scores.legend()
+    eps.legend()
+
+    fig.tight_layout()
+    plt.show()
+
+    save = input('Save? [y/n] ')
+    if save == 'y':
+        index = path.find('/')
+        pdfs = path[:index] + '/pdfs/'
+        os.system(f'mkdir -p {pdfs}')
+        loc = pdfs + path[spi + 1:-4] + '.pdf'
+        print(f'Saving to {loc}...')
+        fig.savefig(loc)
+    print()
+
+def graph_policy_average(path, pol):
+    # Setup the figure
+    fig = plt.figure(figsize = (8, 6))
     trials = fig.add_subplot(1, 1, 1)
-
-    spi = 0
-    for i in range(len(path)):
-        if path[i] == '/':
-            spi = i
-
-    title = path[spi + 1:]
-    fig.suptitle(f'Statistics For Environment {title}')
-
-    # Collect averages
-    avgs = [file_averages(path + '/' + file) for file in files if file != 'TS_Tutoring.csv']
-
-    tutoring_averages(path + '/TS_Tutoring.csv')
-
-    # Assume the lengths are the same
-    tlen = len(avgs[0])
-
-    # Episodes for each
-    teps = [i + 1 for i in range(tlen)]
-
-    # Plot
-    for i in range(len(avgs)):
-        trials.plot(teps, avgs[i], label = (files[i])[:-4].replace('_', ' '))
 
     # Axis labels
     trials.set_ylabel('Trial Scores')
     trials.set_xlabel('Episode')
+
+    # Plot
+    tscores_avg, h = get_policy_average(path, pol)
+    episodes = range(1, len(tscores_avg) + 1)
+    p = trials.plot(episodes, tscores_avg, label = pol)
+    trials.fill_between(episodes, np.subtract(tscores_avg, h),
+        np.add(tscores_avg, h), color = p[0].get_color(), alpha = 0.2)
 
     # Legends
     trials.legend()
@@ -171,6 +195,41 @@ def graph_full_environment(path, files):
     fig.tight_layout()
     plt.show()
 
+    # TODO: put in another function
+    save = input('Save? [y/n] ')
+    if save == 'y':
+        index = path.find('/')
+        pdfs = path[:index] + '/pdfs/'
+        os.system(f'mkdir -p {pdfs}')
+        loc = pdfs + path[spi + 1:] + '.pdf'
+        print(f'Saving to {loc}...')
+        fig.savefig(loc)
+    print()
+
+def graph_environment_averages(path, dirs):
+    # Setup the figure
+    fig = plt.figure(figsize = (8, 6))
+    trials = fig.add_subplot(1, 1, 1)
+
+    # Axis labels
+    trials.set_ylabel('Trial Scores')
+    trials.set_xlabel('Episode')
+
+    # Plotting
+    for pol in dirs:
+        tscores_avg, h = get_policy_average(path, pol)
+        episodes = range(1, len(tscores_avg) + 1)
+        p = trials.plot(episodes, tscores_avg, label = pol)
+        trials.fill_between(episodes, np.subtract(tscores_avg, h),
+            np.add(tscores_avg, h), color = p[0].get_color(), alpha = 0.2)
+
+    # Legends
+    trials.legend()
+
+    fig.tight_layout()
+    plt.show()
+
+    # TODO: put in another function
     save = input('Save? [y/n] ')
     if save == 'y':
         index = path.find('/')
@@ -183,32 +242,58 @@ def graph_full_environment(path, files):
 
 def graph_environment(path):
     # List of directories
-    files = [f for f in os.listdir(path) if f.endswith('.csv')]
+    dirs = [d for d in os.listdir(path)]
 
     print('\n' + '-' * 23)
     print('Available data files:')
     print('-' * 23)
 
-    for i in range(len(files)):
-            print(f'[{i + 1}] {files[i]}')
+    for i in range(len(dirs)):
+            print(f'[{i + 1}] {dirs[i]}')
 
     # Flush with a newline
     print()
     while True:
             index = input('Select a file using its index: ')
 
+            commas = index.split(',')
             if index == 'q' or index == 'quit':
                 break
-            elif index == 'a' or index == 'all':
-                graph_full_environment(path, files)
+            elif len(commas) > 1:
+                ndirs = []
+                fail = False
+                for cm in commas:
+                    i = int(cm)
+                    if i < 1 or i > len(dirs):
+                        print('\tIndex is out of bounds\n')
+                        fail = True
+                        break
+                    else:
+                        ndirs.append(dirs[i - 1])
 
+                if fail:
+                    continue
+
+                graph_environment_averages(path, ndirs)
+                continue
+            elif index == 'a' or index == 'all':
+                graph_environment_averages(path, dirs)
+
+                continue
+            elif index[-1] == 'a':
+                i = int(index[:-1])
+
+                graph_policy_average(path, dirs[i - 1])
                 continue
 
             index = int(index)
-            if index < 1 or index > len(files):
+            if index < 1 or index > len(dirs):
                 print('\tIndex is out of bounds\n')
             else:
-                graph_file(path + '/' + files[index - 1])
+                if dirs[index - 1] == 'TS_Tutoring':
+                    graph_ts_policy(path + '/' + dirs[index - 1])
+                else:
+                    graph_policy(path + '/' + dirs[index - 1])
 
 def graph_directory(path):
     subdirs = [f.name for f in os.scandir(path) if f.is_dir()]
@@ -232,14 +317,7 @@ def graph_directory(path):
 
 if __name__ == "__main__":
     # List of directories
-    directories = []
-
-    # Extract the directories
-    regex = re.compile('results*')
-    for root, dirs, files in os.walk("."):
-        for dir in dirs:
-            if regex.match(dir):
-                directories.append(dir)
+    directories = [d for d in os.listdir('.') if d.startswith('results_')]
 
     directories.sort(reverse = True)
     if len(directories) == 1:
